6a7e90c3bfa9458327992e4c92f21c184c785171
kaua <kauadasilvas02@gmail.com>
2026-09-13 21:48:55 -0300

    inicio do programa

--- Jogo_de_perguntas.java
+++ Jogo_de_perguntas.java
@@ -4,6 +4,8 @@ public class Jogo_de_perguntas{
     public static void main(String []args){
         Scanner sc = new Scanner(System.in);
 
-        
+        int resposta = sc.nextInt();
+
+        System.out.println("Jogo de respostas");
     }
 }
