dcf922fbb68cc62a72c1bca83790c238664176e0
kaua <kauadasilvas02@gmail.com>
2026-09-13 22:44:33 -0300

    if else trocado por switch e inserido while

--- Jogo_de_perguntas.java
+++ Jogo_de_perguntas.java
@@ -10,20 +10,29 @@
             int ponto = 0;
             int erro = 0;
 
+            do{
                     System.out.println("1 - Qual é a capital do brasil?");
                     System.out.println("1. Brasilia");
                     System.out.println("2. São Paulo");
                     System.out.println("3. Rio de Janeiro");
 
             resposta = sc.nextInt();
-        
-            if(resposta == 1){
-                System.out.println("Acertou");
-                ponto++;
-            } else{
-                System.out.println("Errou");
-                erro++;
+
+            switch(resposta){
+                case 1:
+                    System.out.println("Acertou");
+                    ponto++;
+                break;
+                case 2:
+                    System.out.println("Errou");
+                break;
+                case 3:
+                    System.out.println("Errou");
+                break;
+                default:
+                    System.out.println("Opção inválida, tente novamente!!");
             }
+        }   while(resposta >= 4);
 
             System.out.println("2 - Quem era Julio Cesar");
             System.out.println("1. Imperador");
@@ -40,10 +49,39 @@
                 erro++;
             }
 
+            System.out.println("3 - Em que ano o Flamengo foi treinado pelo JJ");
+            System.out.println("1. 2018");
+            System.out.println("2. 2022");
+            System.out.println("3. 2019");
+
+            resposta = sc.nextInt();
+
+            if(resposta == 3){
+                System.out.println("Acertou");
+                ponto++;
+            } else{
+                System.out.println("Errou");
+                erro++;
+            }
+
+
+
             resposta = ponto + erro;
+
+            System.out.println("");
+
+            System.out.println("====Resultado====");
 
             System.out.println("Você tem " + ponto + " ponto(s)");
             System.out.println("Você tem " + erro + " erro(s)");
             System.out.println("Perguntas totais " + resposta);
+
+            System.out.println("");
+
+            if(ponto >= 2){
+                System.out.println("Fez o minimo esperado");
+            } else{
+                System.out.println("PQP");
+            }
         }
     }
