1b425a9d39e9219cd77602b436cb3840f1ea350c
kaua <kauadasilvas02@gmail.com>
2026-09-13 22:01:36 -0300

    Primeiro acerto do jogo

--- Jogo_de_perguntas.java
+++ Jogo_de_perguntas.java
@@ -4,8 +4,20 @@ public class Jogo_de_perguntas{
     public static void main(String []args){
         Scanner sc = new Scanner(System.in);
 
-        int resposta = sc.nextInt();
+        System.out.println("====Jogo de respostas====");
 
-        System.out.println("Jogo de respostas");
+        int resposta;
+        int ponto = 0;
+
+                System.out.println("1 - Qual é a capital do brasil?");
+                System.out.println("1. Brasilia");
+                System.out.println("2. São Paulo");
+                System.out.println("3. Rio de Janeiro");
+
+        resposta = sc.nextInt();
+        
+        if(resposta == 1){
+            System.out.println("Acertou");
+        }
     }
 }
